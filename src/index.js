e9166db89f40c0a30da7ce1a48de27ebd6113dea
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-10-17 22:04:32 +0300

    add furnitureController

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import furnitureController from "./controllers/furnitureController.js";
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("test")
-})
+    res.send([]);
+});
+app.use(furnitureController);
 
 app.listen(3030, () => console.log("Server is listening on http://localhost:3030..."));
